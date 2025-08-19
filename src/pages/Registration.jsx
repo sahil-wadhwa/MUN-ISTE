00b290e3744fabd18fec6cd3f83e927c343798e2
sahil-wadhwa <sahil06.wadhwa@gmail.com>
2025-08-19 10:20:30 +0530

    Update Registration.jsx

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -91,7 +91,7 @@ const Registration = () => {
 
     try {
       const response = await fetch(
-        "https://iste-mun-backend.vercel.app/api/register",
+        "https://iste-mun-backend.onrender.com",
         {
           method: "POST",
           headers: {
